ce4dc8fae3a1f69af317a3d9eadbeb979b7e039d
renatocantarino <renato.cantarino@gmail.com>
2021-05-05 12:07:29 -0300

    mvcApp

---
+++ src/NerdStore.WebApp.MVC/Setup/IoC.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data.Contexts;
+using NerdStore.Catalogo.Data.Repositories;
+using NerdStore.Catalogo.Domain.Repositories;
+using NerdStore.Catalogo.Domain.Services;
+using NerdStore.SharedKernel.EventHandlers;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class IoC
+    {
+        public static void RegisterServices(this IServiceCollection service)
+        {
+            service.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            //catalogo
+            service.AddScoped<IProdutoRepository, ProdutoRepositorio>();
+            service.AddScoped<IProdutoAppService, ProdutoAppService>();
+            service.AddScoped<IEstoqueService, EstoqueService>();
+            service.AddScoped<CatalogoContext>();
+        }
+    }
+}
---
+++ src/NerdStore.WebApp.MVC/Startup.cs
@@ -0,0 +1,71 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NerdStore.Catalogo.Application.Mappers;
+using NerdStore.Catalogo.Data.Contexts;
+using NerdStore.WebApp.MVC.Data;
+using NerdStore.WebApp.MVC.Setup;
+
+namespace NerdStore.WebApp.MVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CatalogoContext>(options => options.UseInMemoryDatabase("CatalogoDB"));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<CatalogoContext>();
+
+            services.AddControllersWithViews();
+
+            services.AddAutoMapper(typeof(DomainToViewModelMappingProfile),
+                                                        typeof(ViewModelToDomainMappingProfile));
+
+            services.AddMediatR(typeof(Startup));
+
+            IoC.RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
