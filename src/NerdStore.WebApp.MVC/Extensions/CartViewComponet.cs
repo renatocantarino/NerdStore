0aa668d2b82a21f922f087e00c5ba4220984d953
renatocantarino <renato.cantarino@gmail.com>
2021-05-09 17:56:45 -0300

    cart view

---
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponet.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IPedidoQueries _pedidoQuery;
+
+        public CartViewComponent(IPedidoQueries pedidoQuery) => _pedidoQuery = pedidoQuery;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cart = await _pedidoQuery.ObterCarrinhoDoCliente(Guid.NewGuid());
+            var itens = cart?.Items.Count ?? 0;
+
+            return View(itens);
+        }
+    }
+}
